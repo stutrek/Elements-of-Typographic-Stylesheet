38fafcb4678e37404352833c60a57ab78dd4ad73
Stu Kabakoff <sakabako@gmail.com>
2012-07-28 19:29:08 -0400

    removed logs

--- content.js
+++ content.js
@@ -5,13 +5,11 @@ function isEnabled() {
 }
 
 function enable(){
-	console.log('enabling');
 	delete localStorage[localStorageName];
 	document.documentElement.classList.add('eotss_enabled');
 };
 
 function disable(){
-	console.log('disabling');
 	localStorage[localStorageName] = true;
 	document.documentElement.classList.remove('eotss_enabled');
 };
@@ -24,7 +22,6 @@ function toggle() {
 	}
 }
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-	console.log('message', arguments );
     if (request.type === "toggle"){
         toggle();
         sendResponse( {'enabled': isEnabled()} );
