89dd79eeff1e8b2bf6f4adb7126ab20ed5594b01
Stu Kabakoff <sakabako@gmail.com>
2012-07-30 20:21:48 -0400

    fixed iframe issues

--- content.js
+++ content.js
@@ -1,40 +1,50 @@
-var localStorageName = 'eotss_'+window.location.host;
-
-function isEnabled() {
-	return !localStorage[localStorageName];
-}
-
-function enable(){
-	delete localStorage[localStorageName];
-	document.documentElement.classList.add('eotss_enabled');
-};
-
-function disable(){
-	localStorage[localStorageName] = true;
-	document.documentElement.classList.remove('eotss_enabled');
-};
-
-function toggle() {
-	if (isEnabled()) {
-		disable();
-	} else {
-		enable();
+(function() {
+	var localStorageName = 'eotss_'+window.location.host;
+	
+	function isEnabled() {
+		return document.documentElement.classList.contains('eotss_enabled');
 	}
-}
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-    if (request.type === "toggle"){
-        toggle();
-        sendResponse( {'enabled': isEnabled()} );
-        return;
-    }
-    if (request.type === "getEnabled") {
-    	sendResponse( {'enabled': isEnabled()} );
-    	return;
-    }
-});
-
-if (isEnabled()) {
-	enable();
-} else {
-	disable();
-}
+	
+	function enable(){
+		try {
+			delete localStorage[localStorageName];
+		} catch (e) {
+		}
+		document.documentElement.classList.add('eotss_enabled');
+	};
+	
+	function disable(){
+		try {
+			localStorage[localStorageName] = true;
+		} catch (e) {
+		}
+		document.documentElement.classList.remove('eotss_enabled');
+	};
+	
+	function toggle() {
+		if (isEnabled()) {
+			disable();
+		} else {
+			enable();
+		}
+	}
+	chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+	    if (request.type === "toggle"){
+	        toggle();
+	        sendResponse( {'enabled': isEnabled(), 'url': window.location.href} );
+	        return;
+	    }
+	    if (request.type === "getEnabled") {
+	        sendResponse( {'enabled': isEnabled(), 'url': window.location.href} );
+	    	return;
+	    }
+	    console.log(request, sender);
+	    throw new Error('Unknown message type! '+request.type);
+	});
+	
+	if (isEnabled()) {
+		enable();
+	} else {
+		disable();
+	}
+}());
