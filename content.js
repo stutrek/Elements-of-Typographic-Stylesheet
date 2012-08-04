6462ca0b5ad0091091c427e9ffb032b88446baa0
Stu Kabakoff <sakabako@gmail.com>
2012-08-04 15:07:53 -0400

    renamed iconController, use a background script instead of a page. Save enabled/disable info in controller rather than content

--- content.js
+++ content.js
@@ -1,50 +1,24 @@
 (function() {
-	var localStorageName = 'eotss_'+window.location.host;
-	
-	function isEnabled() {
-		return document.documentElement.classList.contains('eotss_enabled');;
-	}
-	
 	function enable(){
-		try {
-			delete localStorage[localStorageName];
-		} catch (e) {
-		}
 		document.documentElement.classList.add('eotss_enabled');
 	};
 	
 	function disable(){
-		try {
-			localStorage[localStorageName] = true;
-		} catch (e) {
-		}
 		document.documentElement.classList.remove('eotss_enabled');
 	};
 	
-	function toggle() {
-		if (isEnabled()) {
-			disable();
-		} else {
-			enable();
-		}
-	}
 	chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-	    if (request.type === "toggle"){
-	        toggle();
-	        sendResponse( {'enabled': isEnabled(), 'url': window.location.href} );
+	    if (request.type === "enable"){
+	        enable();
+	        sendResponse( {'url': window.location.href} );
 	        return;
 	    }
-	    if (request.type === "getEnabled") {
-	        sendResponse( {'enabled': isEnabled(), 'url': window.location.href} );
-	    	return;
+	    if (request.type === "disable"){
+	        disable();
+	        sendResponse( {'url': window.location.href} );
+	        return;
 	    }
 	    console.log(request, sender);
 	    throw new Error('Unknown message type! '+request.type);
 	});
-	
-	if (!localStorage[localStorageName]) {
-		enable();
-	} else {
-		disable();
-	}
 }());
