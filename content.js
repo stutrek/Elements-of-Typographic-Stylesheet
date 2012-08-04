6db4efff713c10cb641f8dfa213da40e0b741c2f
Stu Kabakoff <sakabako@gmail.com>
2012-08-04 16:43:04 -0400

    Initial commit. Working.

---
+++ content.js
@@ -0,0 +1,21 @@
+(function() {
+	function enable(){
+		document.documentElement.classList.add('eotss_enabled');
+	};
+	
+	function disable(){
+		document.documentElement.classList.remove('eotss_enabled');
+	};
+	
+	safari.self.addEventListener( 'message', function(message){
+		console.log(message);
+	    if (message.name === "enable"){
+	        enable();
+	        return;
+	    }
+	    if (message.name === "disable"){
+	        disable();
+	        return;
+	    }
+	}, false);
+}());
