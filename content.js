22c573446a12780da48dd05b996b26a864ce129e
Stu Kabakoff <sakabako@gmail.com>
2013-10-20 19:05:39 -0400

    fix memory leak in new safari

--- content.js
+++ content.js
@@ -14,13 +14,13 @@
 			document.documentElement.classList.add('eotss_slow');
 		}, 0);
 		delete localStorage[KEY];
-	};
+	}
 	
 	function disable(){
 		localStorage[KEY] = '0';
 		document.documentElement.classList.remove('eotss_enabled');
 		document.documentElement.classList.remove('eotss_slow');
-	};
+	}
 	
 	function messageReciever(message){
 	    if (message.name === "enable"){
@@ -32,9 +32,15 @@
 	        return;
 	    }
 	}
+	function removeReceiver() {
+		safari.self.removeEventListener( 'message', messageReciever, false );
+		window.removeEventListener( 'beforeunload', removeReceiver, true );
+	}
 	
 	if (isSafari) {
+		console.log(window.safari);
 		safari.self.addEventListener( 'message', messageReciever, false);
+		window.addEventListener( 'beforeunload', removeReceiver, true);
 	} else {
 		chrome.extension.onMessage.addListener(messageReciever);
 	}
